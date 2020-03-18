01c7301b343d79e86235963f2844ff870c6975c1
Kenefiss <kenefiss@gmail.com>
2020-03-18 02:43:16 +0200

    added animation and slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,18 @@
 $(function () {
 
+  $('[data-counter]').counter({
+    decimals: 0,
+    decPoint: ".",
+    thousandsSep: ""
+  });
 
+  new WOW().init({
+    mobile: true,
+  });
 
-
-
-
+  $('.testimonials__items').slick({
+    dots: true,
+    arrows: false
+  });
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,9 @@ gulp.task('sass', function () {
 gulp.task('style', function () {
   return gulp.src([
       'node_modules/normalize.css/normalize.css',
-      'node_modules/slick-carousel/slick/slick.css'
+      'node_modules/slick-carousel/slick/slick.css',
+      'node_modules/animate.css/animate.css'
+
     ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
@@ -36,7 +38,9 @@ gulp.task('style', function () {
 
 gulp.task('script', function () {
   return gulp.src([
-      'node_modules/slick-carousel/slick/slick.js'
+      'node_modules/slick-carousel/slick/slick.js',
+      'node_modules/jquery-counter/dist/jquery.counter.min.js',
+      'node_modules/wow.js/dist/wow.js'
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
